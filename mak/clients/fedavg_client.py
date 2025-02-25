6a69ee9b68b3d34ae4c8d134b24abf2ecac134ad
Nguyễn Khoa <42170045+neko941@users.noreply.github.com>
2025-02-25 14:41:44 +0900

    format the repo

--- mak/clients/fedavg_client.py
+++ mak/clients/fedavg_client.py
@@ -8,8 +8,13 @@ class FedAvgClient(BaseClient):
     """
     Simple flwr client implementation using basic fedavg approach
     """
-    def __init__(self, client_id, model, trainset, valset, config_sim, device, save_dir):
-        super().__init__(client_id, model, trainset, valset, config_sim, device, save_dir)
+
+    def __init__(
+        self, client_id, model, trainset, valset, config_sim, device, save_dir
+    ):
+        super().__init__(
+            client_id, model, trainset, valset, config_sim, device, save_dir
+        )
 
     def __repr__(self) -> str:
         return " FedAvg client"
